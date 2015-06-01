23c8581e5568d808cdb7fa75ac8488d1e9b97268
Lee <andersonsoftworks@gmail.com>
2015-06-01 12:13:25 -0400

    Basic Skeleton

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+//This anonymous function calls itself, and generates the entire script inside the function's 
+//////scope, to prevent accidental overwriting of data in the "Window" Global Scope
+(function( window, undefined ){
+
+	//javascript code goes here
+
+} )( window, undefined );
+
